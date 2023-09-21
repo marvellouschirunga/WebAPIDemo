274049f2f872db69e3df0dffece934505093dc4f
marvellouschirunga <marvellouschirunga@gmail.com>
2023-09-21 09:52:55 +0200

    Add project files.

---
+++ WebAPIDemo/Controllers/PantsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PantsController: ControllerBase
+    {
+        [HttpGet]
+        public string GetPants()
+        {
+            return "Reading all pants";
+        }
+
+        [HttpGet("{id}")]
+        public string GetPantByID(int id)
+        {
+            return $"Reading pant with ID: {id}";
+        }
+
+        [HttpPost]
+        public string CreatePant()
+        {
+            return "Creating pant";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdatePant(int id) 
+        {
+            return $"Updating pant with ID: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeletePant(int id) 
+        {
+            return $"Deleting pant with ID: {id}";
+        }
+    }
+}
---
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ShirtsController: ControllerBase
+    {
+        [HttpGet]
+        public string GetShirts()
+        {
+            return "Reading all the shirts";
+        }
+
+        [HttpGet("{id}")]
+        public string GetShirtByID(int id)
+        {
+            return $"Reading Shirt: {id}";
+        }
+
+        [HttpPost]
+        public string CreateShirt([FromBody]Shirt shirt)
+        {
+            return "Creating a shirt";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdateShirt(int id) 
+        {
+            return $"Updating shirt: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteShirt(int id) 
+        { 
+            return $"Deleting shirt: {id}";
+        }
+    }
+}
